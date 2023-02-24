0aaeb8a877e972dfb6142d540ef13a23dd5fa8fb
DI0m11 <115796443+DI0m11@users.noreply.github.com>
2023-02-24 16:11:10 +0000

    Initial commit

---
+++ app/components/navbar/NavbarLink.tsx
@@ -0,0 +1,9 @@
+import { Link } from "@remix-run/react";
+const NavbarLink = () => {
+  return (
+    <Link to="#" className="block text-white">
+      Test
+    </Link>
+  );
+};
+export default NavbarLink;
