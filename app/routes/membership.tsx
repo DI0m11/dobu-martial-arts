69458acc42de3ad629a2e760db53ba2aebb21b29
DI0m11 <115796443+DI0m11@users.noreply.github.com>
2023-04-28 15:10:56 +0100

    Add component to memberships page

--- app/routes/membership.tsx
+++ app/routes/membership.tsx
@@ -1,122 +1,51 @@
+interface MembershipCardProps {
+  level: string;
+  description: string;
+  price: string;
+}
+
+function MembershipCard({ level, description, price }: MembershipCardProps) {
+  return (
+    <li className="mb-4 rounded-lg bg-white p-4 text-center shadow-lg transition-all last-of-type:mb-0 hover:scale-105 hover:cursor-pointer">
+      <h2 className="mb-4 font-serif text-2xl font-extrabold tracking-wide text-green-900">
+        {level}
+      </h2>
+      <dl>
+        <dt className="bold mb-1 text-xs uppercase text-amber-900">
+          Description
+        </dt>
+        <dd className="mb-3 text-neutral-600">{description}</dd>
+        <dt className="bold mb-1 text-xs uppercase text-amber-900">Price</dt>
+        <dd className="mb-3 font-serif text-xl font-extrabold text-red-800">
+          {price}
+        </dd>
+      </dl>
+    </li>
+  );
+}
+
 export default function Memberships() {
   return (
-    <div className="bg-white-200 min-h-screen p-4">
-      <h1 className="text-red mb-4 text-center font-serif text-4xl font-extrabold">
-        Memberships
+    <div className="min-h-screen bg-yellow-100 p-4">
+      <h1 className="mb-4 text-center font-serif text-4xl font-extrabold text-red-900">
+        Membership Page
       </h1>
       <ul className="mx-auto max-w-xl p-4">
-        <li className="mb-4 rounded-lg bg-white p-4 text-center shadow-lg transition-all last-of-type:mb-0 hover:scale-105 hover:cursor-pointer">
-          <h2 className="mb-4 font-serif text-2xl font-extrabold tracking-wide text-red-700">
-            Basic
-          </h2>
-          <dl>
-            <dt className="bold mb-1 text-xs uppercase text-amber-900">
-              Description
-            </dt>
-            <dd className="mb-3 text-neutral-600">
-              1 martial art - 2 sessions per week. Monthly fee.
-            </dd>
-            <dt className="bold mb-1 text-xs uppercase text-amber-900">
-              Price
-            </dt>
-            <dd className="mb-3 font-serif text-xl font-extrabold text-red-700">
-              £25.00
-            </dd>
-          </dl>
-        </li>
-        <li className="bg-red mb-4 rounded-lg p-4 text-center shadow-lg transition-all last-of-type:mb-0 hover:scale-105 hover:cursor-pointer">
-          <h2 className="mb-3 font-serif text-2xl font-extrabold tracking-wide text-red-700">
-            Intermediate
-          </h2>
-          <dl>
-            <dt className="bold mb-1 text-xs uppercase text-amber-900">
-              Description
-            </dt>
-            <dd className="mb-3 text-neutral-600">
-              1 martial art - 3 sessions per week. Monthly fee.
-            </dd>
-            <dt className="bold mb-1 text-xs uppercase text-amber-900">
-              Price
-            </dt>
-            <dd className="mb-3 font-serif text-xl font-extrabold text-red-700">
-              £35.00
-            </dd>
-          </dl>
-        </li>
-        <li className="mb-4 rounded-lg bg-white p-4 text-center shadow-lg transition-all last-of-type:mb-0 hover:scale-105 hover:cursor-pointer">
-          <h2 className="mb-3 font-serif text-2xl font-extrabold tracking-wide text-red-700">
-            Advanced
-          </h2>
-          <dl>
-            <dt className="bold mb-1 text-xs uppercase text-amber-900">
-              Description
-            </dt>
-            <dd className="mb-3 text-neutral-600">
-              Any 2 martial arts - 5 sessions per week. Monthly fee.
-            </dd>
-            <dt className="bold mb-1 text-xs uppercase text-amber-900">
-              Price
-            </dt>
-            <dd className="mb-3 font-serif text-xl font-extrabold text-red-700">
-              £45.00
-            </dd>
-          </dl>
-        </li>
-        <li className="mb-4 rounded-lg bg-white p-4 text-center shadow-lg transition-all last-of-type:mb-0 hover:scale-105 hover:cursor-pointer">
-          <h2 className="mb-3 font-serif text-2xl font-extrabold tracking-wide text-red-700">
-            Elite
-          </h2>
-          <dl>
-            <dt className="bold mb-1 text-xs uppercase text-amber-900">
-              Description
-            </dt>
-            <dd className="mb-3 text-neutral-600">Unlimited classes.</dd>
-            <dt className="bold mb-1 text-xs uppercase text-amber-900">
-              Price
-            </dt>
-            <dd className="mb-3 font-serif text-xl font-extrabold text-red-700">
-              £60.00
-            </dd>
-          </dl>
-        </li>
-        <li className="mb-4 rounded-lg bg-white p-4 text-center shadow-lg transition-all last-of-type:mb-0 hover:scale-105 hover:cursor-pointer">
-          <h2 className="mb-3 font-serif text-2xl font-extrabold tracking-wide text-red-700">
-            Private Martial Arts Tuition
-          </h2>
-          <dl>
-            <dt className="bold mb-1 text-xs uppercase text-amber-900">
-              Description
-            </dt>
-            <dd className="mb-3 text-neutral-600">
-              Private lessons for any martial art.
-            </dd>
-            <dt className="bold mb-1 text-xs uppercase text-amber-900">
-              Price
-            </dt>
-            <dd className="mb-3 font-serif text-xl font-extrabold text-red-700">
-              £15.00
-            </dd>
-          </dl>
-        </li>
-        <li className="mb-4 rounded-lg bg-white p-4 text-center shadow-lg transition-all last-of-type:mb-0 hover:scale-105 hover:cursor-pointer">
-          <h2 className="mb-3 font-serif text-2xl font-extrabold tracking-wide text-red-700">
-            Junior Membership
-          </h2>
-          <dl>
-            <dt className="bold mb-1 text-xs uppercase text-amber-900">
-              Description
-            </dt>
-            <dd className="mb-3 text-neutral-600">
-              Can attend all-kids martial arts sessions.
-            </dd>
-            <dt className="bold mb-1 text-xs uppercase text-amber-900">
-              Price
-            </dt>
-            <dd className="mb-3 font-serif text-xl font-extrabold text-red-700">
-              £25.00
-            </dd>
-          </dl>
-        </li>
+        <MembershipCard
+          level="Basic"
+          description="1 martial art - 2 sessions per week. Monthly fee."
+          price="£25.00"
+        />
+        <MembershipCard
+          level="Intermediate"
+          description="1 martial art - 3 sessions per week. Monthly fee."
+          price="£35.00"
+        />
+        <MembershipCard
+          level="Advanced"
+          description="Any 2 martial arts - 5 sessions per week. Monthly fee."
+          price="£45.00"
+        />
       </ul>
     </div>
   );
