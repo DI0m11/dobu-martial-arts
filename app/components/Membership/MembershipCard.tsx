5e35783796add9bfab07c3fde3fd91483de900a4
DI0m11 <115796443+DI0m11@users.noreply.github.com>
2023-05-01 15:26:52 +0100

    membership

--- app/components/Membership/MembershipCard.tsx
+++ app/components/Membership/MembershipCard.tsx
@@ -11,15 +11,15 @@ export default function MembershipCard({
 }: MembershipCardProps) {
   return (
     <li className="mb-4 rounded-lg bg-white p-4 text-center shadow-lg transition-all last-of-type:mb-0 hover:scale-105 hover:cursor-pointer">
-      <h2 className="mb-4 font-serif text-2xl font-extrabold tracking-wide text-green-900">
+      <h2 className="mb-4 font-serif text-2xl font-extrabold tracking-wide text-red-900">
         {level}
       </h2>
       <dl>
-        <dt className="bold mb-1 text-xs uppercase text-amber-900">
+        <dt className="bold text-black-900 mb-1 text-xs uppercase">
           Description
         </dt>
         <dd className="mb-3 text-neutral-600">{description}</dd>
-        <dt className="bold mb-1 text-xs uppercase text-amber-900">Price</dt>
+        <dt className="bold text-black-900 mb-1 text-xs uppercase">Price</dt>
         <dd className="mb-3 font-serif text-xl font-extrabold text-red-800">
           {price}
         </dd>
--- app/routes/membership.tsx
+++ app/routes/membership.tsx
@@ -1,5 +1,6 @@
 import MembershipCard from "~/components/Membership/MembershipCard";
 import { LoaderArgs } from "@remix-run/node";
+import { useUser } from "~/utils";
 
 import { requireUserId } from "~/session.server";
 
@@ -30,6 +31,22 @@ export default function Memberships() {
           level="Advanced"
           description="Any 2 martial arts - 5 sessions per week. Monthly fee."
           price="£45.00"
+        />
+        <MembershipCard
+          level="Elite"
+          description="Unlimited classes"
+          price="£60.00"
+        />
+        <MembershipCard
+          level="Private Martial Arts Tuition"
+          description="Private one to one sessions"
+          price="£15.00"
+        />
+        <MembershipCard
+          level="Junior Membership"
+          description="Can attend all-kids martial arts
+          sessions"
+          price="£25.00"
         />
       </ul>
     </div>
